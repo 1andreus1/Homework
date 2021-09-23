cb8108c997a152c45f14b9d0738b07ebb0be52a9
1andreus1 <79913205+1andreus1@users.noreply.github.com>
2021-09-23 23:50:51 +0300

    update

--- hw3/2.cpp
+++ hw3/2.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 #include <vector>
 
+void Merge(std::vector <int>& x, int l, int r, int m) {
+    std::vector < int > res;
+    auto c1 = l;
+    auto c2 = m + 1;
+    while (c1 <= m && c2 <= r) {
+
+        if (x[c1] < x[c2]) {
+            res.push_back(x[c1]);
+            c1++;
+        }
+        else {
+            res.push_back(x[c2]);
+            c2++;
+        }
+    }
+
+    if (c1 <= m) {
+        while (c1 <= m) {
+            res.push_back(x[c1]);
+            c1++;
+        }
+    }
+    if (c2 <= r) {
+        while (c2 <= r) {
+            res.push_back(x[c2]);
+            c2++;
+        }
+    }
+    for (auto i = l; i <= r; ++i) {
+        x[i] = res[i - l];
+    }
+}
+
+
 void MergeSort(std::vector <int>& x, int l, int r) {
 
     if (l == r) {
@@ -12,47 +46,17 @@ void MergeSort(std::vector <int>& x, int l, int r) {
         }
     }
     else {
-        std::vector < int > res;
-        int m = (l + r) / 2;
+        auto m = (l + r) / 2;
         MergeSort(x, l, m);
         MergeSort(x, m + 1, r);
-        int c1 = l;
-        int c2 = m + 1;
-        while (c1 <= m && c2 <= r) {
-
-            if (x[c1] < x[c2]) {
-                res.push_back(x[c1]);
-                c1++;
-            }
-            else {
-                res.push_back(x[c2]);
-                c2++;
-            }
-        }
-
-        if (c1 <= m) {
-            while (c1 <= m) {
-                res.push_back(x[c1]);
-                c1++;
-            }
-        }
-        if (c2 <= r) {
-            while (c2 <= r) {
-                res.push_back(x[c2]);
-                c2++;
-            }
-        }
-        for (int i = l; i <= r; ++i) {
-            x[i] = res[i - l];
-        }
+        Merge(x, l, r, m);
     }
 }
 
-int main()
-{
+int main() {
     std::vector < int > a = { 5,4,3,2,1 };
     MergeSort(a, 0, a.size() - 1);
-    for (int i = 0; i < a.size(); ++i) {
-        std::cout << a[i] << ' ';
+    for (int i : a) {
+        std::cout << i << ' ';
     }
 }
